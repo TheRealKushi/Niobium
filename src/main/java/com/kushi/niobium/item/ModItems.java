c847443731cf289912960ce05c42bf7ff4c96c0c
TheRealKushi <danielysousasantana@gmail.com>
2024-12-26 18:52:09 -0300

    Updated files

--- src/main/java/com/kushi/niobium/item/ModItems.java
+++ src/main/java/com/kushi/niobium/item/ModItems.java
@@ -2,6 +2,7 @@ package com.kushi.niobium.item;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import com.kushi.niobium.Niobium;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
@@ -9,7 +10,8 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    public static final Item LIGHT_HELMET = registerItem("light_helmet", new Item(new Item.Settings()));
+    public static final Item LIGHT_HELMET = registerItem("light_helmet", new ArmorItem(ModArmorMaterials.LIGHT_HELMET_MATERIAL, ArmorItem.Type.HELMET, new Item.Settings()
+            .maxDamage(ArmorItem.Type.HELMET.getMaxDamage(15))));
     public static final Item IRON_TILE = registerItem("iron_tile", new Item(new Item.Settings()));
     public static final Item ENDRITE_SCRAP = registerItem("endrite_scrap", new Item(new Item.Settings()));
 
